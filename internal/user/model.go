aa461f7a196500186f5d3db2fd5fb25a1bff8baf
AlexCorn999 <alexcorn999@list.ru>
2023-07-25 18:07:03 +0300

    decomposed all code

--- internal/user/model.go
+++ internal/user/model.go
@@ -5,52 +5,36 @@ import (
 )
 
 // count of users
-var User_id = 1
+var UserID = 1
 
 type User struct {
-	Name    string  `json: "name"`
-	Age     string  `json: "age"`
+	name    string  `json: "name"`
+	age     string  `json: "age"`
 	Friends []*User `json: "friends"`
 }
 
+// UserCreated оповещает о добавлении пользователя.
 func (u *User) UserCreated() string {
-	return fmt.Sprintf("User was created %s\nUser_id : %d.\n", u.Name, User_id)
+	return fmt.Sprintf("User was created %s\nUserID : %d.\n", u.name, UserID)
 }
 
+// NowFriends оповещает о дружбе двух пользователей.
 func NowFriends(u1, u2 *User) string {
-	return fmt.Sprintf("%s и %s теперь друзья.", u1.Name, u2.Name)
+	return fmt.Sprintf("%s и %s теперь друзья.", u1.name, u2.name)
 }
 
+// UserDeleted оповещает об удалении пользователя.
 func (u *User) UserDeleted() string {
-	return fmt.Sprintf("User %s has been deleted", u.Name)
+	return fmt.Sprintf("User %s has been deleted", u.name)
 }
 
+// Выводит информацию о друге у пользователя.
 func (u *User) FriendsToString() string {
-	return fmt.Sprintf("Name is %s and age is %s\n", u.Name, u.Age)
+	return fmt.Sprintf("Name is %s and age is %s\n", u.name, u.age)
 }
 
-func (u *User) NewAge() string {
-	return fmt.Sprintf("Возраст пользователя %s успешно обновлен на %s", u.Name, u.Age)
-}
-
-func (u *User) DeleteFromFriends() {
-	var allFriends []*User
-
-	// adding friends of the user we want to delete
-	for _, friend := range u.Friends {
-		allFriends = append(allFriends, friend)
-	}
-
-	// deleting a user from each friend
-	for i := 0; i < len(allFriends); i++ {
-
-		for j := 0; j < len(allFriends[i].Friends); j++ {
-
-			if allFriends[i].Friends[j] == u {
-				newFriends := append(allFriends[i].Friends[:j], allFriends[i].Friends[j+1:]...)
-				allFriends[i].Friends = newFriends
-			}
-
-		}
-	}
+// NewAge меняет возраст пользователя.
+func (u *User) NewAge(age string) string {
+	u.age = age
+	return fmt.Sprintf("Возраст пользователя %s успешно обновлен на %s", u.name, u.age)
 }
